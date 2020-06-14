a22a550e35b1687275d1b6e128442e6ac5c78e77
Beloy Andrey <bagmobile@yandex.ru>
2020-06-15 00:15:26 +0300

    Личный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {movieDescription} = props;
+  const {movieDescription, moviesList} = props;
+
   return (
     <Main
       description={movieDescription}
+      movies={moviesList}
     />
   );
+};
+
+App.propTypes = {
+  movieDescription: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired
+  }),
+  moviesList: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,33 @@ const MovieDescription = {
   year: 2014
 };
 
+const movies = [
+  `Fantastic Beasts: The Crimes of Grindelwald`,
+  `Bohemian Rhapsody`,
+  `Macbeth`,
+  `Aviator`,
+  `We need to talk about Kevin`,
+  `What We Do in the Shadows`,
+  `Revenant`,
+  `Johnny English`,
+  `Shutter Island`,
+  `Pulp Fiction`,
+  `No Country for Old Men`,
+  `Snatch`,
+  `Moonrise Kingdom`,
+  `Seven Years in Tibet`,
+  `Midnight Special`,
+  `War of the Worlds`,
+  `Dardjeeling Limited`,
+  `Orlando`,
+  `Mindhunter`,
+  `Midnight Special`
+];
+
 ReactDOM.render(
     <App
       movieDescription={MovieDescription}
+      moviesList={movies}
     />,
     document.querySelector(`#root`)
 );
